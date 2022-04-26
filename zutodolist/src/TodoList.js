7691c1f9fe07fffbc7d3c05ffa7e0d495717026e
sumin <shohom38@naver.com>
2022-04-26 23:01:36 +0900

    make zustand todolist

---
+++ zutodolist/src/TodoList.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { useState } from "react";
+import { TodoStore } from "./TodoStore";
+
+function TodoList() {
+    const [todoValue, setTodoValue] = useState("");
+}
---
+++ zutodolist/src/TodoStore.js
@@ -0,0 +1,35 @@
+import create from "zustand/react";
+import { uuid } from "react-uuid";
+
+const TodoStore = create((set) => ({
+  todos: [],
+  addTodo: (todoText) => set((state) => ({
+      todos: [
+      ...state.todos,
+      {
+        text: todoText,
+        id: uuid(`${todoText} - ${state.todos.length}`),
+        isDone: false
+      }
+    ]
+  })),
+  deleteTodo: (todoId) => 
+  set((state) => ({
+    todos: state.todos.filter((todo) => todo.id !== todoId)
+  })),
+  doneTodo: (todoId) =>
+  set((state) => ({
+    todos: state.todos.map((todo) => {
+      if(todo.id === todoId) {
+        return {
+          ...todo,
+          isDone: true
+        };
+      }
+
+      return todo;
+    })
+  }))
+}))
+
+export default TodoStore;
