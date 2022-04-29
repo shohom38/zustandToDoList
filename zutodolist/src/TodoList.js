b41606b34f12ea0d39612f6e7674de41b165ffdc
sumin <shohom38@naver.com>
2022-04-30 00:44:22 +0900

    make todo list modified btn

--- zutodolist/src/TodoList.js
+++ zutodolist/src/TodoList.js
@@ -1,9 +1,10 @@
+import React from "react";
 import { useState } from "react";
 import useTodoStore from "./useTodoStore";
 
 function TodoList() {
   const [todoValue, setTodoValue] = useState("");
-  const { todos, addTodo, deleteTodo } = useTodoStore(
+  const { todos, addTodo, deleteTodo, modifiedTodo } = useTodoStore(
     (state) => state
   );
   const handleSubmit = (e) => {
@@ -19,12 +20,25 @@ function TodoList() {
               <input 
                 type="text"
                 id="newTodo"
-                // value="new-todo"
                 onChange={(e) => setTodoValue(e.target.value)}
                 name="doSomthing"
               />
               <button type="submit">Add Todo</button>
           </form>
+
+          <ul>
+            {todos.map((todo) => {
+              return (
+                <li key={todo.id}>
+                  <span>
+                    {todo.text}{" "}
+                  </span>
+                  <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+                  <button onClick={() => modifiedTodo(todo.id)}>Modified</button>
+                </li>
+              )
+            })}
+          </ul>
       </div>
   )
 }
